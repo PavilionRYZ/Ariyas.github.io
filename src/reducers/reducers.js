b62d44443302daf199a590e81044d6f35e722c1e
Sayan Modak <sayanprodev111@gmail.com>
2024-02-13 22:34:38 +0530

    Fixed the react router bug, created shop page and also updated the folde structure

--- src/reducers/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import apiReducer from './../Slices/apiSlice';
-
-const rootReducer = combineReducers({
-  api: apiReducer,
-})
-
-export default rootReducer;
